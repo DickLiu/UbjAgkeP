41979c380865a7f3b745c152729f5a8696fdcd5f
Dick Liu <dick.liu@citiesocial.com>
2020-03-12 19:38:34 +0800

    add_rfm_view

--- x_2/orderly/orderly/urls.py
+++ x_2/orderly/orderly/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from rfm import views as rfm_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('rfm-sort/', rfm_views.rfm_sort),
 ]
--- x_2/orderly/rfm/views.py
+++ x_2/orderly/rfm/views.py
@@ -1,4 +1,39 @@
-from django.shortcuts import render
+import os
 
-# Create your views here.
+from django.http import JsonResponse
 
+import pandas as pd
+
+
+def rfm_sort(request):
+
+    sort_by = request.GET.get('sort_by', "recency")
+
+    module_dir = os.path.dirname(__file__)
+
+    file_path = os.path.join(module_dir, r'static\rfm\rfm.csv')
+
+    dtypes = {'ID': 'int',
+              'R': 'int',
+              'F': 'int',
+              'M': 'int'}
+
+    table = pd.read_csv(file_path,
+                        sep=',',
+                        header=0,
+                        dtype=dtypes)
+
+    table.loc[:, "RFM"] = table["R"] + table["F"] + table["M"]
+
+    if sort_by == "recency":
+        _values = table.sort_values(by=['R', 'ID'], ascending=[False, True])["ID"].values
+        return JsonResponse(_values.tolist(), safe=False)
+    elif sort_by == "frequency":
+        _values = table.sort_values(by=['F', 'ID'], ascending=[False, True])["ID"].values
+        return JsonResponse(_values.tolist(), safe=False)
+    elif sort_by == "monetary":
+        _values = table.sort_values(by=['M', 'ID'], ascending=[True, True])["ID"].values
+        return JsonResponse(_values.tolist(), safe=False)
+    elif sort_by == "rfm":
+        _values = table.sort_values(by=['RFM', 'ID'], ascending=[False, True])["ID"].values
+        return JsonResponse(_values.tolist(), safe=False)
