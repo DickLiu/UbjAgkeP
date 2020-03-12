e5c37aba38839be0d9e39f53d9068a49a106a9b8
Dick Liu <dick.liu@citiesocial.com>
2020-03-12 19:50:10 +0800

    add_docstring_of_rfm_sort

--- x_2/orderly/rfm/views.py
+++ x_2/orderly/rfm/views.py
@@ -6,6 +6,19 @@ import pandas as pd
 
 
 def rfm_sort(request):
+    """
+    根據request物件中的sort_by parameter去進行rfm.csv檔案的id排序
+    eg. http://xxx.xxx.com/rfm-sort/?sort_by=recency
+
+    如果sort_by是 recency, 將 ID 以倒 R 值排序回傳 (大到小)
+    如果sort_by是 frequency, 將 ID 以倒 F 值排序回傳 (大到小)
+    如果sort_by是 monetary, 將 ID 以 M 值排序回傳 (小到大)
+    如果sort_by是 rfm, 將 ID 以倒 RFM 總值排序回傳
+    ps. 如果排序對象數值相同, 再以id由小到大的順序排列
+
+    :param request:
+    :return: json
+    """
     sort_by = request.GET.get('sort_by', "recency")
 
     module_dir = os.path.dirname(__file__)
